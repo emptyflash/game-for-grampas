eb2b86d0f26ee790ebc001a78619e27a0dd8cef1
Cameron Alexander <emptyflash@gmail.com>
2016-07-06 05:54:13 -0500

    major changes, extrememly basic gameplay done

--- player.gd
+++ player.gd
@@ -1,30 +1,88 @@
 
-extends KinematicBody2D
+extends RigidBody2D
 
-var velocity = Vector2()
-var points = []
-const speed = 100
+const SPEED = 2000
+const TIME_UNIT = 1
+const rooms = ["Lobby", "Hallways", "Shower", "Cafe", "Bedroom"]
+const needs = ["Hungry", "Dirty", "Sleepy"]
+const need_dict = {
+	"Hungry": "Cafe",
+	"Dirty": "Shower",
+	"Sleepy": "Bedroom"
+}
+
+var path = []
+var selected = false
+var destination = null
+var time_acc = 0
+var satisfaction = 50
+var need = needs[randi() % needs.size()]
+
 
 func _ready():
 	set_fixed_process(true)
-	
-func _fixed_process(delta):
-	points = get_node("../Navigation2D").get_simple_path(get_global_pos(), get_global_mouse_pos(), false)
-	if points.size() >= 1:
-		velocity = (points[1] - get_global_pos()).normalized()
+	set_process_unhandled_input(true)
+	var roomLabel = Label.new()
+	roomLabel.set_text(need)
+	add_child(roomLabel)
 
-	var motion = velocity * speed * delta
-	move(motion)
+func _fixed_process(delta):
+	if destination != null:
+		if destination.distance_to(get_global_pos()) < 5:
+			destination = null
+			path = []
+		else:
+			path = get_node("../Navigation2D").get_simple_path(get_global_pos(), destination, false)
+
+	if path.size() >= 1:
+		var dist = path[1] - get_global_pos()
+		var impulse = dist.normalized() * SPEED
+		apply_impulse(Vector2(), impulse * delta)
 	
-	if (is_colliding()):
-		var n = get_collision_normal()
-		motion = n.slide(motion)
-		velocity = n.slide(velocity)
-		move(motion)
+	if time_acc >= TIME_UNIT:
+		tick_time_unit()
+		time_acc = 0
 	
+	time_acc += delta
 	update()
 
 func _draw():
-	if points.size() > 1:
-		for p in points:
-			draw_circle(p - get_global_pos(), 8, Color(1, 0, 0))
+	if path.size() > 1:
+		for i in range(1, path.size()):
+			var p1 = path[i - 1] - get_global_pos()
+			var p2 = path[i] - get_global_pos()
+			draw_line(p1, p2, Color(0, 1, 0))
+	get_node("Satisfaction").set_text("%d" % satisfaction)
+
+func _unhandled_input(event):
+	if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and event.is_pressed():
+		var dist = (get_global_mouse_pos() - get_global_pos()).length()
+		if dist < 30:
+			selected = true
+		else:
+			selected = false
+	elif event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_RIGHT and event.is_pressed():
+		if selected:
+			destination = get_global_mouse_pos()
+
+
+func tick_time_unit():
+	var satisfy_delta = -1
+	
+	if get_current_room() == need_to_room(need):
+		satisfy_delta += 2
+	
+	satisfaction += satisfy_delta
+
+func need_to_room(need):
+	return need_dict[need]
+
+func check_room(room_node_name):
+	var cafe = get_node("../" + room_node_name)
+	var mapPos = cafe.world_to_map(get_global_pos())
+	return cafe.get_cellv(mapPos) >= 0
+
+func get_current_room():
+	for room_name in rooms:
+		if check_room(room_name):
+			return room_name
