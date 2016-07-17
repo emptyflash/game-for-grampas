3c265a5a7bb582b26146ebc70eab6a1fd690cc19
Cameron Alexander <emptyflash@gmail.com>
2016-07-17 16:43:08 -0700

    Add animations and script function to play them

--- player.gd
+++ player.gd
@@ -1,4 +1,3 @@
-
 extends Node2D
 
 const SPEED = 100
@@ -14,12 +13,30 @@ func _ready():
 func _fixed_process(delta):
 	if path.size() > 0:
 		var dist = path[0] - get_global_pos()
+		play_animation_for_direction(dist.normalized())
 		var velocity = dist.normalized() * SPEED
 		translate(velocity * delta)
-		if dist.length() < 15:
+		if dist.length() < 2:
 			path.pop_front()
+	else:
+		play_animation_for_direction(Vector2())
 
 	update()
+
+func play_animation_for_direction(direction):
+	var player = get_node("Sprite/AnimationPlayer")
+	var animation_name = "idle"
+	if direction.x > 0 and direction.y > 0:
+		animation_name = "walk_down_right"
+	elif direction.x > 0 and direction.y < 0:
+		animation_name = "walk_up_right"
+	elif direction.x < 0 and direction.y > 0:
+		animation_name = "walk_down_left"
+	elif direction.x < 0 and direction.y < 0:
+		animation_name = "walk_up_left"
+	
+	if player.get_current_animation() != animation_name:
+		player.play(animation_name)
 
 func _draw():
 	if path.size() > 1:
@@ -28,7 +45,7 @@ func _draw():
 			var p2 = path[i] - get_global_pos()
 			draw_line(p1, p2, Color(0, 1, 0))
 	if selected:
-		draw_circle(get_pos() - get_global_pos(), 15, Color(0, 0, 1))
+		draw_circle(get_pos() - get_global_pos(), 10, Color(0, 0, 1))
 
 func _unhandled_input(event):
 	if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT and event.is_pressed():
